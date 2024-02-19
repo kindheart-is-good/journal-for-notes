a955a547269e88f9b761d501761acb22381d61d3
metaphysical_light <metacat.me@gmail.com>
2024-02-20 02:50:39 +0300

    Сreated CardButton component by implementing composition.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import CardButton from "./components/CardButton/CardButton";
 import JournalItem from "./components/JournalItem/JournalItem";
 
 function App() {
@@ -20,11 +21,13 @@ function App() {
     <>
       <h1>Starting</h1>
       <Button />
-      <JournalItem
-        title={data[0].title}
-        date={data[0].date}
-        text={data[0].text}
-      />
+      <CardButton>
+        <JournalItem
+          title={data[0].title}
+          date={data[0].date}
+          text={data[0].text}
+        />
+      </CardButton>
     </>
   );
 }
--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -6,13 +6,13 @@ function JournalItem({ title, date, text }) {
   const formattedDate = new Intl.DateTimeFormat("ru-RU").format(date);
 
   return (
-    <div className="journal-item">
+    <>
       <h2 className="journal-item__header">{title}</h2>
       <h2 className="journal-item__body">
         <div className="journal-item__date">{formattedDate}</div>
         <div className="journal-item__text">{text}</div>
       </h2>
-    </div>
+    </>
   );
 }
 
