48cc0266bdd29507698f3f5c4eae66c52a9946b8
metaphysical_light <metacat.me@gmail.com>
2024-02-22 17:46:46 +0300

    Added validation of data inside form.

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -3,14 +3,11 @@ import "./JournalForm.css";
 import Button from "./../Button/Button";
 
 function JournalForm({ onSubmit }) {
-  const [inputData, setInputData] = useState("");
-
-  const inputChange = (e) => {
-    //console.log(e);
-    console.log(e.target.value);
-    console.log(inputData);
-    setInputData(e.target.value);
-  };
+  const [formValidState, setFormValidState] = useState({
+    title: true,
+    post: true,
+    date: true,
+  });
 
   const addJournalItem = (e) => {
     e.preventDefault(); // для того чтобы не уходило обновление страницы, а вместо этого просто нажималась кнопка и туда передавался e
@@ -18,22 +15,50 @@ function JournalForm({ onSubmit }) {
     const formData = new FormData(e.target); //из FormData API
     const formProps = Object.fromEntries(formData);
 
+    let isFormValid = true;
+    if (!formProps.title?.trim().length) {
+      setFormValidState((state) => ({ ...state, title: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState((state) => ({ ...state, title: true }));
+    }
+    if (!formProps.post?.trim().length) {
+      setFormValidState((state) => ({ ...state, post: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState((state) => ({ ...state, post: true }));
+    }
+    if (!formProps.date) {
+      setFormValidState((state) => ({ ...state, date: false }));
+      isFormValid = false;
+    } else {
+      setFormValidState((state) => ({ ...state, date: true }));
+    }
+    if (!isFormValid) {
+      return;
+    }
     //console.log(formProps);
     onSubmit(formProps);
   };
 
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" />
-      <input type="date" name="date" />
-      <input type="text" name="tag" value={inputData} onChange={inputChange} />
-      <textarea name="text"></textarea>
-      <Button
-        text="Save"
-        onClick={() => {
-          console.log("Clicked!");
-        }}
+      <input
+        type="text"
+        name="title"
+        style={{ border: formValidState.title ? undefined : "1px solid red" }}
       />
+      <input
+        type="date"
+        name="date"
+        style={{ border: formValidState.date ? undefined : "1px solid red" }}
+      />
+      <input type="text" name="tag" />
+      <textarea
+        name="post"
+        style={{ border: formValidState.post ? undefined : "1px solid red" }}
+      ></textarea>
+      <Button text="Save" />
     </form>
   );
 }
