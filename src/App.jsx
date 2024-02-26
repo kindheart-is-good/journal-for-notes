8d76309c8fc55699a44d3d55a5d94b1502929eef
metaphysical_light <metacat.me@gmail.com>
2024-02-26 23:57:23 +0300

    Moved data to localStorage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
@@ -7,29 +7,32 @@ import JournalList from "./components/JournalList/JournalList";
 import Body from "./layouts/Body/Body";
 import LeftPanel from "./layouts/LeftPanel/LeftPanel";
 
-const INITIAL_DATA = [
-  // {
-  //   id: 1,
-  //   title: "Item",
-  //   text: "Some text",
-  //   date: new Date(),
-  // },
-  // {
-  //   id: 2,
-  //   title: "Item2",
-  //   text: "Some text one more",
-  //   date: new Date(),
-  // },
-];
-
 function App() {
-  const [items, setItems] = useState(INITIAL_DATA);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("data"));
+    if (data) {
+      setItems(
+        data.map((item) => ({
+          ...item,
+          date: new Date(item.date),
+        }))
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (items.length) {
+      localStorage.setItem("data", JSON.stringify(items));
+    }
+  }, [items]);
 
   const addItem = (item) => {
     setItems((oldItems) => [
       ...oldItems,
       {
-        text: item.text,
+        post: item.post,
         title: item.title,
         date: new Date(item.date),
         id:
